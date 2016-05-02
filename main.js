8a8e5470ab1535466a9555fad7436420f339d0ca
Cory Tohill <corytohill@gmail.com>
2016-05-02 13:46:19 -0500

    changed js to use forEach method instead of for loops

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ dogFoodRequest.open("GET", "dogFood.json");
 dogFoodRequest.send();
 dogFoodRequest.addEventListener("load", processPetFoodData);
 
+
 var catFoodRequest = new XMLHttpRequest();
 catFoodRequest.open("GET", "catFood.json");
 catFoodRequest.send();
@@ -23,7 +24,7 @@ catFoodRequest.addEventListener("load", processPetFoodData);
 function processPetFoodData () {
   var petFoodData = JSON.parse(this.responseText);
 
-  for (i = 0; i < petFoodData.brands.length; i++) {
+  petFoodData.brands.forEach(function(name, i){
     brandHTML += petFoodData.brands[i].name;
 
     //determines whether a breeds section exists and gathers appropriate data if it does, defaults to "All Breeds" if no data found
@@ -32,37 +33,35 @@ function processPetFoodData () {
         var breeds = `${petFoodData.brands[i].breeds[bre].breed}, `;
         breeds = breeds[0].toUpperCase() + breeds.slice(1);
         breedsHTML += breeds;
-      }
+      };
     } else {
       breedsHTML += `All Breeds`;
-    }
+    };
     breedsDiv.innerHTML += `<p>${breedsHTML}</p>`;
     breedsHTML = "";
 
-    // determins number of brands and inserts them into appropriate divs in HTML
-    for (typ = 0; typ < petFoodData.brands[i].types.length; typ++) {
+    // determins types of food and formats the text
+    petFoodData.brands[i].types.forEach(function(type, typ) {
       typesHTML += `${petFoodData.brands[i].types[typ].type}`;
       typesHTML = typesHTML[0].toUpperCase() + typesHTML.slice(1);
       typesHTML = typesHTML.replace("_", " ");
 
-      // determines the types, sizes, and prices and inserts them into the appropriate divs in HTML
-      for (vol = 0; vol < petFoodData.brands[i].types[typ].volumes.length; vol++) {
-        nameHTML = petFoodData.brands[i].types[typ].volumes[vol].name;
+      // determines the sizes, and prices associated with a specific type of food and inserts them into the appropriate divs in HTML
+      petFoodData.brands[i].types[typ].volumes.forEach(function (name, vol){
+        var volume = petFoodData.brands[i].types[typ].volumes[vol];
 
-        priceHTML = petFoodData.brands[i].types[typ].volumes[vol].price;
-
+        nameHTML = volume.name;
+        priceHTML = volume.price;
         typeVolumePriceHTML += `${typesHTML}, ${nameHTML}, ${priceHTML} : `;
-      }
+      });
       typesHTML = "";
-
-    }
+    });
+    //removes the extra ": " at the end of the typeVolumePriceHTML before instering into the DOM and then resets the variable
     typeVolumePriceHTML = typeVolumePriceHTML.slice(0, -2);
     volumesPricesDiv.innerHTML += `<p>${typeVolumePriceHTML}</p>`;
     typeVolumePriceHTML = "";
 
     brandDiv.innerHTML += `<p>${brandHTML}</p>`;
     brandHTML = "";
-  }
-
-
+  });
 };
